494ddc7d61e6acd0ae49059b9cc61067615fae92
makos <matmakos@gmail.com>
2011-03-02 17:12:39 +0100

    fixed error

--- eightball.py
+++ eightball.py
@@ -3,13 +3,13 @@
 import random
 import re
 
-eightball = ("Yes, {}.", "No, {}.", "Who knows?", "Maybe, {}.", "How should I know?", "That's very possible, {}.", "How about no.", "42.", \
+eightballdb = ("Yes, {}.", "No, {}.", "Who knows?", "Maybe, {}.", "How should I know?", "That's very possible, {}.", "How about no.", "42.", \
              "No, you piece of shit.", "{} no ecchi!")
 
 def eightball( user ):
     """See the future."""
 
-    return random.choice(eightball).format(user)
+    return random.choice(eightballdb).format(user)
 
 if __name__ == '__main__':
 	print eightball( "fatapaca" )
